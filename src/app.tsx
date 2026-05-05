a011a4a7bcbae5de4eaa590b687757292b6068a7
Lucas Sotero <lucas.abreu2@fit-tecnologia.org.br>
2026-05-04 21:06:32 -0400

    feat(logo): adicionando logo e estilizando

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
 import { CreateLink } from "./components/create-link";
+import { LogoApp } from "./components/logo-app";
 
 export function App() {
   return (
     <main className="h-dvh flex flex-col items-center justify-center p-10">
+      <LogoApp></LogoApp>
       <CreateLink></CreateLink>
     </main>
   )
---
+++ src/components/logo-app.tsx
@@ -0,0 +1,11 @@
+export function LogoApp() {
+    return (
+        <div className="flex flex-row gap-1 mb-10">
+            <img src="/src/assets/images/subtract.svg" />
+            <h1 className="font-logo font-bold text-[18.67px] text-[#2C46B1]">brev.ly</h1>
+        </div>
+    )
+}
+
+
+
